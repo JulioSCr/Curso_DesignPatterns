350483aedd54a8978aeb67255148ac9264ce0351
Julio Silva Cruz <julioscruzny@gmail.com>
2020-08-13 19:02:36 -0300

    Exercícios Strategy

---
+++ CursoDesignPatterns/Strategy/ICCC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class ICCC : IImposto
+    {
+        public decimal Calcular(Orcamento pOrcamento)
+        {
+            if (pOrcamento.Valor < 1000.00m)
+                return pOrcamento.Valor * 0.05m;
+
+            if (pOrcamento.Valor <= 3000 && pOrcamento.Valor >= 1000)
+                return pOrcamento.Valor * 0.07m;
+
+            return (pOrcamento.Valor * 0.08m) + 30;
+        }
+    }
+}
