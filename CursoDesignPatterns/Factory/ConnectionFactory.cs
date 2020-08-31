c67598f7afe93b5f0e8291821e3625e9a24765ce
Julio Silva Cruz <julioscruzny@gmail.com>
2020-08-31 02:13:40 -0300

    Factory

---
+++ CursoDesignPatterns/Factory/ConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Factory
+{
+	public class ConnectionFactory
+	{
+		public IDbConnection GetConnection()
+		{
+			IDbConnection lConexao = new SqlConnection();
+			lConexao.ConnectionString = "User Id=lg;Password=syslg;Server=localhost;Database=AtonCore";
+			lConexao.Open();
+			return lConexao;
+		}
+	}
+}
---
+++ CursoDesignPatterns/Factory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace Factory
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			IDbConnection lConexao = new ConnectionFactory().GetConnection();
+			IDbCommand lComando = lConexao.CreateCommand();
+			lComando.CommandText = "select * from glo.tbPessoa";
+		}
+	}
+}
+///Isola a criação de projetos complicados como o padrão Builder, porém a implementação desses objetos não varia tanto como o Builder.
