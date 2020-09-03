cfcbec51b93c3638cd9919260936cb501c48a543
Julio Silva Cruz <julioscruzny@gmail.com>
2020-09-03 00:20:24 -0300

    Adapter

---
+++ CursoDesignPatterns/Adapter/GeradorXml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Adapter
+{
+	public class GeradorXml
+	{
+		public string GeraXml(object pObjeto)
+		{
+			XmlSerializer lSerializador = new XmlSerializer(pObjeto.GetType());
+			string lXml;
+			using (StringWriter lWriter = new StringWriter())
+			{
+				lSerializador.Serialize(lWriter, pObjeto);
+				lXml = lWriter.ToString();
+			}
+			return lXml;
+		}
+	}
+}
---
+++ CursoDesignPatterns/Adapter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Cliente lCliente = new Cliente("Julio", "Rua Leopardo", DateTime.Now);
+			string lXml = new GeradorXml().GeraXml(lCliente);
+			Console.WriteLine(lXml);
+			Console.ReadLine();
+		}
+	}
+}
+///Utilizamos o adapter quando queremos adaptar o código de um sistema legado ou de uma biblioteca para que ele trabalhe corretamente
+///com o sistema orientado a objetos que está sendo desenvolvido.
